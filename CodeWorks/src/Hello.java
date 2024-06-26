e2c9220e48e0aaa5bdd3bddea18a4628d3434745
RDBKISD <74268036+RDBKISD@users.noreply.github.com>
2024-06-26 14:33:50 -0500

    First Test

---
+++ CodeWorks/src/Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("How this works!  Now csdvsdvghngeds");
+		
+		Teacher t1 = new Teacher();
+		
+		System.out.println(t1);
+
+	}
+
+}
---
+++ CodeWorks/src/Teacher.java
@@ -0,0 +1,23 @@
+
+public class Teacher {
+	
+	private String firstName;
+	private String lastName;
+
+
+	      public Teacher() {
+	    	  
+	      }
+	      
+	      public Teacher(String f, String l) {
+	    	  
+	    	  this.firstName = f;
+	    	  this.lastName = l;
+	      }
+	      
+	      public String toString() {
+	    	  
+	    	  return "I am a Teacher!";
+	      }
+
+}
